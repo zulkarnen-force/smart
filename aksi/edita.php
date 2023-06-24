d9e7181906d1e4b5300b8d435a1440eeb3cf83f6
Zulkarnen <zulkarnen1900016072@webmail.uad.ac.id>
2023-06-24 19:19:03 +0700

    oke

--- aksi/edita.php
+++ aksi/edita.php
@@ -1,16 +1,19 @@
-<form action="update_data.php" method="POST">
+<form action="update.php" method="POST">
 	<?php
 	include '../onek.php';
 	$nik = $_GET['nik'];
-		$query = mysqli_query($dbcon,"SELECT * FROM alternatif WHERE nik = '$nik'");
+	$query = mysqli_query($dbcon,"SELECT * FROM alternatif WHERE nik = '$nik'");
 		$alternatif = mysqli_fetch_assoc($query);
 		var_dump($alternatif);
-		foreach ($alternatif as $value) {	
+		foreach ($alternatif as $key => $value) {	
 	?>
-	<div>
-			<input type="text" name="NewName" placeholder="Name" value="<?= $value ?>">
-	</div>
+
+			<input type="text" name="<?= $key ?>" placeholder="Name" value="<?= $value ?>">
+	
 	<?php } ?>
+	<div>
+	<button type="submit" name="submit">Update Order</button>
+	</div>
 	</form>
 	<?php
 ?>
